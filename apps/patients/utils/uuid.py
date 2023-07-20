ebda37c0afe018388e86fd1763ed81f10ea39782
garusocruz <garusotec@gmail.com>
2023-07-19 22:09:34 -0300

    feat: create utils for uuid

---
+++ apps/patients/utils/uuid.py
@@ -0,0 +1,14 @@
+"""uuid generic functions
+"""
+import uuid
+from pydantic import UUID4
+
+
+@staticmethod
+def generate_uuid() -> UUID4:
+    """generate a UUID4
+
+    Returns:
+        UUID4: uuid4 field
+    """
+    return uuid.uuid4()
