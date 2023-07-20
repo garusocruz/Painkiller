b5a63b8d0c17c9269658d7576026ad10000f980f
garusocruz <garusotec@gmail.com>
2023-07-20 16:39:31 -0300

    refactor: PEP8 in file name

--- apps/patients/schemas/pydantic/patient.py
+++ apps/patients/schemas/pydantic/patient.py
@@ -1,11 +1,11 @@
 """PatientSchema is an interface with Pydantic layer and Patient Prisma ORM Model
 """
 from typing import Optional
-from prisma.models import Patient
+from apps.patients.models.patient import PatientModel
 
 
-class PostPatient(Patient):
-    """PostPatient is an chield of Patient model replacing the patient_id to be an empty field in a primary instance
+class PostPatientSchema(PatientModel):
+    """PostPatientSchema is an chield of Patient model replacing the patient_id to be an empty field in a primary instance
 
     Args:
         Patient (Prisma Model): inherit of Patient Prisma ORM Model
