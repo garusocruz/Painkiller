a8364e7aae3bb4c3d535715c35ab4382af8cefe0
garusocruz <garusotec@gmail.com>
2023-07-19 21:34:56 -0300

    chore: set up prisma at main app

--- apps/patients/core/main.py
+++ apps/patients/core/main.py
@@ -2,10 +2,22 @@
 main module all configurations from fast api application
 """
 from fastapi import FastAPI
+from prisma import Prisma
 
 app = FastAPI(
     title="PainKiller - Patients", 
 )
+prisma = Prisma(auto_register=True)
+
+@app.on_event("startup")
+async def startup() -> None:
+    await prisma.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown() -> None:
+    if prisma.is_connected():
+        await prisma.disconnect()
 
 @app.get('/')
 def index() -> dict:
