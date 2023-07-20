3907d959ea262620e1b60b1c6d7ecc8d6d2d1009
garusocruz <garusotec@gmail.com>
2023-07-19 22:00:15 -0300

    feat: add patients routes

--- apps/patients/core/main.py
+++ apps/patients/core/main.py
@@ -3,11 +3,16 @@ main module all configurations from fast api application
 """
 from fastapi import FastAPI
 from prisma import Prisma
+from ..routers.v1.PatientsRouter import router as patient_router
+
 
 app = FastAPI(
     title="PainKiller - Patients", 
 )
 prisma = Prisma(auto_register=True)
+
+# Add Routers
+app.include_router(patient_router)
 
 @app.on_event("startup")
 async def startup() -> None:
