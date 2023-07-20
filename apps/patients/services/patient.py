bc6943372bb3311e2a1836aa04fbda93d7041d9a
garusocruz <garusotec@gmail.com>
2023-07-20 16:37:31 -0300

    feat: create service layer

---
+++ apps/patients/services/patient.py
@@ -0,0 +1,37 @@
+"""
+Patient Service module put all business logic inside of this file
+"""
+from apps.patients.models.patient import PatientModel
+from apps.patients.schemas.pydantic.patient import PostPatientSchema
+from apps.patients.utils.uuid import generate_uuid
+from apps.patients.interfaces.app import get_orm
+
+
+class PatientService:
+    """PatientService is a proxy layer to query actions in Database"""
+
+    def __init__(self) -> None:
+        """Initializing Prisma ORM"""
+        self.orm = get_orm().orm
+
+    async def create(self, patient: PostPatientSchema) -> PatientModel:
+        """Create a Patient
+
+        Returns:
+            PatientModel: A proxy to Prisma Models
+        """
+        if not patient.patient_id:
+            patient.patient_id = generate_uuid().hex
+
+        return await self.orm.patient.create(patient.dict())
+
+    async def read_by_id(self, patient_id: str) -> PatientModel:
+        """query a patient according by patient_id
+
+        Args:
+            patient_id (str): pk of PatientModel
+
+        Returns:
+            PatientModel: A proxy to Prisma Models
+        """
+        return await self.orm.patient.find_first(where={"patient_id": patient_id})
