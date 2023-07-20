bb634b773d3845a4d1335e6fe8d99a3644a5da5b
garusocruz <garusotec@gmail.com>
2023-07-19 21:15:20 -0300

    docs: adjust docstrings

--- scripts/prisma.py
+++ scripts/prisma.py
@@ -1,14 +1,14 @@
 """
-fast_api module have many scripts to run fast api commands
+prisma module have many scripts to execute ORM commands
 """
 from os import system as shcmd
 
 def migrate_patients() -> None:
     """
-    The function start a fast api server using a uvicorn with arguments like port
+    The function apply the latest migration
     following example:
 
-    poetry run patients
+    prisma migrate dev
     """
 
     shcmd(f"prisma migrate dev --schema=apps/patients/prisma/schema.prisma")
