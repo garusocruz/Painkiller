fc8062ea9a35ecc577f2dc7890c7a6f77e8376e7
garusocruz <garusotec@gmail.com>
2023-07-20 16:24:34 -0300

    refactor: export base default instances of server

---
+++ apps/patients/interfaces/app.py
@@ -0,0 +1,34 @@
+"""Module used to mix Prisma and Fast Api in one Class Object
+"""
+from fastapi import FastAPI
+from apps.patients.database.prisma_orm import PrismaORM
+
+
+class AppInterface:
+    """App Interface is a mixed class Object containing Prisma and Fast Api injected modules"""
+
+    def __init__(self) -> None:
+        """Initialize prisma and fast api properties"""
+        self.prisma = PrismaORM()
+        self.fast_api = FastAPI(docs_url="/", title="PainKiller - Patients")
+
+
+_SERVER = AppInterface()
+
+
+def get_app() -> FastAPI:
+    """Used to export app instance
+
+    Returns:
+        FastAPI: return a Fast Api instance
+    """
+    return _SERVER.fast_api
+
+
+def get_orm() -> PrismaORM:
+    """Used to export Prisma instance
+
+    Returns:
+        PrismaORM: return a PrismaORM abstraction of Prisma module
+    """
+    return _SERVER.prisma
