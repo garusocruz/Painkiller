06c50f07e6e956360ab40848c407010e8d0c7c2f
garusocruz <garusotec@gmail.com>
2023-07-20 18:23:38 -0300

    chore: add cmd to run measurement migrations app

--- scripts/prisma.py
+++ scripts/prisma.py
@@ -3,6 +3,7 @@ prisma module have many scripts to execute ORM commands
 """
 from os import system as shcmd
 
+
 def migrate_patients() -> None:
     """
     The function apply the latest migration
@@ -12,3 +13,14 @@ def migrate_patients() -> None:
     """
 
     shcmd(f"prisma migrate dev --schema=apps/patients/prisma/schema.prisma")
+
+
+def migrate_measurement() -> None:
+    """
+    The function apply the latest migration
+    following example:
+
+    prisma migrate dev
+    """
+
+    shcmd(f"prisma migrate dev --schema=apps/measurement/prisma/schema.prisma")
