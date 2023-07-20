232a05f671380a2acf8c8aa50afe1778bdafe912
garusocruz <garusotec@gmail.com>
2023-07-20 18:22:14 -0300

    chore: create a services to measurement app

---
+++ apps/measurement/services/measurement_service.py
@@ -0,0 +1,26 @@
+"""
+Measurement Service module put all business logic inside of this file
+"""
+from apps.measurement.models.measurement_model import MeasurementModel
+from apps.measurement.schemas.pydantic.measurement_schema import PostMeasurementSchema
+from apps.measurement.utils.uuid import generate_uuid
+from apps.measurement.interfaces.app import get_orm
+
+
+class MeasurementService:
+    """MeasurementService is a proxy layer to query actions in Database"""
+
+    def __init__(self) -> None:
+        """Initializing Prisma ORM"""
+        self.orm = get_orm().orm
+
+    async def create(self, measurement: PostMeasurementSchema) -> MeasurementModel:
+        """Create a Measurement
+
+        Returns:
+            MeasurementModel: A proxy to Prisma Models
+        """
+        if not measurement.measurement_id:
+            measurement.measurement_id = generate_uuid().hex
+
+        return await self.orm.measurement.create(measurement.dict())
