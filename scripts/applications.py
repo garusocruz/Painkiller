b87ea6ab654e0989bb6232a593d17d1b492564e7
garusocruz <garusotec@gmail.com>
2023-07-20 18:23:15 -0300

    chore: add cmd to start measurement app

--- scripts/applications.py
+++ scripts/applications.py
@@ -3,6 +3,7 @@ fast_api module have many scripts to run fast api commands
 """
 from os import system as shcmd
 
+
 def patients_api() -> None:
     """
     The function start a fast api server using a uvicorn with arguments like port
@@ -12,3 +13,14 @@ def patients_api() -> None:
     """
 
     shcmd(f"uvicorn apps.patients.core.main:app --reload")
+
+
+def measurement_api() -> None:
+    """
+    The function start a fast api server using a uvicorn with arguments like port
+    following example:
+
+    poetry run measurements
+    """
+
+    shcmd(f"uvicorn apps.measurement.core.main:app --reload --port 8001")
