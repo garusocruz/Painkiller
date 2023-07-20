2baa3b26cbccaf112b4ff2a9a5e292781e8f06be
garusocruz <garusotec@gmail.com>
2023-07-19 22:15:19 -0300

    feat: create route to query a patient by id

--- apps/patients/routers/v1/PatientsRouter.py
+++ apps/patients/routers/v1/PatientsRouter.py
@@ -26,3 +26,18 @@ async def create(patient: PostPatient) -> Patient:
         patient.patient_id = generate_uuid().hex
 
     return await prisma.patient.create(patient.dict())
+
+
+@router.get("/api/v1/patient/{patient_id}", response_model=Patient)
+async def read_by_id(patient_id: str):
+    """read_by_id method, trigger a DN query instruction to fetch patient row according a patient_id
+
+    Args:
+        patient_id (str): PK of Patient Model
+
+    Returns:
+        Patient: Patient Prisma ORM Model
+    """
+    from ...core.main import prisma
+
+    return await prisma.patient.find_first(where={"patient_id": patient_id})
