a63d710453e28bcb979b755d1748a7180012df2c
garusocruz <garusotec@gmail.com>
2023-07-22 00:09:28 -0300

    feat: change status code

--- src/apps/measurement/routers/v1/measurement_router.py
+++ src/apps/measurement/routers/v1/measurement_router.py
@@ -1,23 +1,20 @@
 """measurement  module
     All Measurement view is aborded bellow
 """
-from fastapi import APIRouter
-from src.apps.measurement.interfaces.app import get_orm
-
-# from src.apps.measurement.models.measurement_model import MeasurementModel, Men
+from fastapi import APIRouter, Response, status
 from src.clients.measurement.models import Measurement
-from src.apps.measurement.schemas.pydantic.measurement_schema import (
+from src.apps.measurement.schemas.measurement_schema import (
     PostMeasurementSchema,
 )
 from src.apps.measurement.services.measurement_service import MeasurementService
 
-orm = get_orm()
+
 router = APIRouter(prefix="/v1/measurements", tags=["measurement"])
 service = MeasurementService()
 
 
 @router.post("/", response_model=Measurement)
-async def create(measurement: PostMeasurementSchema):
+async def create(measurement: PostMeasurementSchema, response: Response):
     """create method, trigger a DB query instruction to create a new Measurement row
 
     Args:
@@ -26,5 +23,6 @@ async def create(measurement: PostMeasurementSchema):
     Returns:
         MeasurementModel: Measurement Prisma Model
     """
-
-    return await service.create(measurement)
+    measurement = await service.create(measurement)
+    response.status_code = status.HTTP_201_CREATED
+    return measurement
